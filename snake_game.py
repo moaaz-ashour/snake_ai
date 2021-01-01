dd7a4d2710ade711dfa8caaa3a4b25c3737388da
ashourmoaaz <ashour.m@live.de>
2021-01-01 14:57:39 +0100

    initializing the game

---
+++ snake_game.py
@@ -0,0 +1,23 @@
+import pygame
+import random
+
+
+
+
+# needed to initialize all modules correctly
+pygame.init()
+
+
+class SnakeGame:
+    def __init__(self, width=640, height=380): # default width and height
+        self.width = width
+        self.height = height
+
+
+
+
+
+
+if __name__ == '__main__':
+    # create a snake game
+    game = SnakeGame()
