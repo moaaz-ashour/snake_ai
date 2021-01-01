84c93780e169af591fdea39f19c69cc09fc6d3e7
ashourmoaaz <ashour.m@live.de>
2021-01-01 15:00:12 +0100

    init game display with width and height

--- snake_game.py
+++ snake_game.py
@@ -1,20 +1,16 @@
 import pygame
 import random
 
-
-
-
 # needed to initialize all modules correctly
 pygame.init()
-
 
 class SnakeGame:
     def __init__(self, width=640, height=380): # default width and height
         self.width = width
         self.height = height
 
-
-
+        # init display with width and height
+        self.display = pygame.display.set_mode(self.width, self.height)
 
 
 
