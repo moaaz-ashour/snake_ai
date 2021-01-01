8af79bbfd71900adc0ce5748c1ff6870fd9d0312
ashourmoaaz <ashour.m@live.de>
2021-01-01 15:52:20 +0100

    init game direction - defining an enumeration

--- snake_game.py
+++ snake_game.py
@@ -1,8 +1,17 @@
 import pygame
 import random
+from enum import Enum
 
 # needed to initialize all modules correctly
 pygame.init()
+
+class Direction(Enum):
+    # CONSTANTS enumeration members
+    RIGHT = 1
+    LEFT = 2
+    UP = 3
+    DOWN = 4
+
 
 class SnakeGame:
     def __init__(self, width=640, height=380): # default width and height
@@ -14,7 +23,10 @@ class SnakeGame:
         # control speed of the game
         self.clock = pygame.time.Clock()
 
-        
+        # init game state:
+        # 1. initial direction:
+        self.direction = Direction.RIGHT
+
 if __name__ == '__main__':
     # create snake game
     game = SnakeGame()
